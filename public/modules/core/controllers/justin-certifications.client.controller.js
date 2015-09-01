51737b7b7ec7d4a9fef87efa6f42b1a6081843ca
Justin Murdock <jmurdock@lyris.com>
2015-09-01 06:54:19 -0700

    add workExperience/Certifications

---
+++ public/modules/core/controllers/justin-certifications.client.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('core').controller('JustinCertificationsController', ['$scope',
+	function($scope) {
+		// Justin certifications controller logic
+		// ...
+	}
+]);
---
+++ public/modules/core/controllers/justin-work-experience.client.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('core').controller('JustinWorkExperienceController', ['$scope',
+	function($scope) {
+		// Justin work experience controller logic
+		// ...
+	}
+]);
