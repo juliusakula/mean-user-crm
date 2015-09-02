e58ce4b7cca4a4e9cb5d6815a7df9b19ad1be1d7
Justin Murdock <jmurdock@lyris.com>
2015-09-02 05:44:46 -0700

    new views for work experience

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -8,6 +8,10 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 
 		// Home state routing
 		$stateProvider.
+		state('projects', {
+			url: '/projects',
+			templateUrl: 'modules/core/views/projects.client.view.html'
+		}).
 		state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
--- public/modules/core/controllers/justin-work-experience.client.controller.js
+++ public/modules/core/controllers/justin-work-experience.client.controller.js
@@ -4,5 +4,7 @@ angular.module('core').controller('JustinWorkExperienceController', ['$scope',
 	function($scope) {
 		// Justin work experience controller logic
 		// ...
+        $scope.company = "Lyris / Aurea, Inc";
+        $scope.role = "Intern, Jr Software Engineer";
 	}
 ]);
